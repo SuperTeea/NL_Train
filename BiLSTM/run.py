c2a7cce8590b10d9d28ce5913ac6248e86f335dc
SuperTeea <super_teaer@qq.com>
2025-10-26 16:00:20 +0800

    feat : add run script and finish a train procedure

---
+++ BiLSTM/run.py
@@ -0,0 +1,14 @@
+from Trainer import Trainer
+from ConllDataset import ConllDataset
+from BiLSTMnet import BiLSTM
+from Vocab import Vocab
+from torch.optim import SGD
+
+if __name__ == '__main__':
+    train_dataset = ConllDataset(r'D:\Codes\NL_Train\Data\little-data\train.conll')
+    vocab = Vocab(train_dataset)
+    train_dataset.confTrans(vocab.encode)   # u1s1我的这种设计模式耦合多，还是蛮糟糕的，先凑合
+    model = BiLSTM(64, 64, vocab.wordSize(), vocab.tagSize())
+    optimizer = SGD(model.parameters(), lr = 0.01)
+    trainer = Trainer(model, optimizer)
+    trainer.fit(train_dataset,batchsize=8, epoches=32)
