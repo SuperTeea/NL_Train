a808b7f77f99909c6eaab164cdacc087de121eaf
SuperTeea <super_teaer@qq.com>
2025-10-26 18:31:35 +0800

    feat : a bit tuning

--- BiLSTM/run.py
+++ BiLSTM/run.py
@@ -5,14 +5,14 @@ from Vocab import Vocab
 from torch.optim import SGD
 
 if __name__ == '__main__':
-    train_dataset = ConllDataset(r'..\Data\little-data\train.conll')
-    test_dataset = ConllDataset(r'..\Data\little-data\dev.conll')
-    # train_dataset = ConllDataset(r'..\Data\little-data\dev.conll')
-    # test_dataset = ConllDataset(r'..\Data\little-data\train.conll')
+    train_dataset = ConllDataset(r'..\Data\ctb5-postagged\train')
+    test_dataset = ConllDataset(r'..\Data\ctb5-postagged\test')
+    # train_dataset = ConllDataset(r'..\Data\little-data\train.conll')
+    # test_dataset = ConllDataset(r'..\Data\little-data\dev.conll')
     vocab = Vocab(train_dataset)
     train_dataset.confTrans(vocab.encode)   # u1s1我的这种设计模式耦合多，还是蛮糟糕的，先凑合
     test_dataset.confTrans(vocab.encode)   
-    model = BiLSTM(128, 128, vocab.wordSize(), vocab.tagSize())
-    optimizer = SGD(model.parameters(), lr = 0.5)
+    model = BiLSTM(256, 128, vocab.wordSize(), vocab.tagSize())
+    optimizer = SGD(model.parameters(), lr = 0.6)
     trainer = Trainer(model, optimizer)
-    trainer.fit(train_dataset,batchsize=8, epoches=64, test_dataset=test_dataset)
+    trainer.fit(train_dataset,batchsize=32, epoches=64, test_dataset=test_dataset)
